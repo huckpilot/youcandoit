ca85712ec5b9b6acbe0c819371afe8ebf0be3049
huckpilot <huckpilot@gmail.com>
2018-12-10 22:52:59 -0800

    Update environment file and image service

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class ImageService {
+  private apiPath: string;
 
   constructor(private http: HttpClient) {
+    this.apiPath = environment.api;
   }
 
   public getImages(slug: string) {
