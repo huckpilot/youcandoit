ec1471180183f16c9d81cdec43287bb754561177
huckpilot <huckpilot@gmail.com>
2019-12-04 22:37:17 -0800

    Remove pw protection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
-import { ImageService } from './services/image.service';
-import {Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,26 +13,10 @@ export class AppComponent implements OnInit {
   title = 'app';
 
   constructor(
-    private router: Router,
-    private imgSrv: ImageService,
-    private angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) {
-      this.angulartics2GoogleAnalytics.startTracking();
-      this.subscription = this.imgSrv.checkIfPwAdded().subscribe(pwAdded => {
-        this.added = pwAdded;
-      });
+    private angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics
+  ) {
+    this.angulartics2GoogleAnalytics.startTracking();
   }
 
-  ngOnInit() {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        if (this.router.url === "/work/animation/color-styling/cleopatra") {
-          if (!this.added) {
-            this.imgSrv.sendForm(true);
-          }
-        } else {
-          this.imgSrv.sendForm(false);
-        }
-      }
-    });
-  }
+  ngOnInit() {}
 }
--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,49 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  Router,
-  ActivatedRoute
-} from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import {Subscription} from 'rxjs';
-import { ImageService } from 'src/app/services/image.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
 @Component({
-  selector: 'app-gallery',
-  templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  selector: "app-gallery",
+  templateUrl: "./gallery.component.html",
+  styleUrls: ["./gallery.component.scss"]
 })
 export class GalleryComponent implements OnInit {
   subscription: Subscription;
   images: any;
   galExpanded: boolean;
   protect = false;
-  values = '';
+  values = "";
   regex: any;
   pw: string;
   result: any;
   stored: string;
 
-  onKey(event: any) { // without type info
-    this.regex = /^2019$/;
-    this.result = this.regex.test(event.target.value);
-    this.values = event.target.value;
-    if (this.result) {
-      localStorage.setItem('passwordAdded', 'true');
-      this.stored = localStorage.getItem('passwordAdded');
-      this.imgSrv.pwAdded(true);
-      this.protect = false;
-    }
-  }
-
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-    private http: HttpClient,
-    private imgSrv: ImageService,
-  ) {
-    this.subscription = this.imgSrv.getForm().subscribe(showForm => {
-      this.protect = showForm;
-    });
-  }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.data.subscribe(data => {
